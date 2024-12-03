88fc6d7916cdec72250a7ba8ace2d901cfeca19b
VerzaElisa <elisaverza.gm@gmail.com>
2024-12-03 15:28:44 +0100

    close #5

--- src/caballo/domestico/wwsimulator/model.py
+++ src/caballo/domestico/wwsimulator/model.py
@@ -1,32 +1,116 @@
+error = 'index out of range'
 class Job():
     """
     A unit of work to be processed by the nodes in a simulation.
     """
-    def __init__(self, class_id: str, job_id: int):
-        self._class_id = class_id
-        self._job_id = job_id
-        @property
-        def class_id(self):
-            return self._class_id
+    def __init__(self, class_id: int, job_id: int):
+        self.class_id = class_id
+        self.job_id = job_id
 
-        @class_id.setter
-        def class_id(self, value):
-            self._class_id = value
+    def class_id(self):
+        return self.class_id
 
-        @property
-        def job_id(self):
-            return self._job_id
+    def class_id(self, value):
+        self.class_id = value
+
+    def job_id(self):
+        return self.job_id
     
-# TODO: this is a mock
-class Network:
-    pass
 
-# TODO: this is a mock
-class Server:
-    pass
-
-# TODO: this is a mock
 class State:
-    pass
+    """
+    Classe che definisce lo stato del sistema con una matrice 3x3 
+    dove ogni riga rappresenta un nodo e ogni colonna una classe di job
+    """
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def update(self, node_class: tuple, increment: bool):
+        """
+        Metodo per incrementare o decrementare lo stato di un nodo
+        """
+
+        if node_class[1] > 3 or node_class[0] > 3:
+            raise ValueError(error)
+        
+        self.matrix[node_class[0]][node_class[1]] += 1 if increment else -1
+
+    def get(self):
+        """
+        Metodo per ritornare la matrice completa dello stato
+        """
+        return self.matrix
+    
+    def get_node_state(self, node):
+        """
+        Metodo per ritornare lo stato di un singolo nodo
+        """
+        return self.matrix[node]
+    
+    def get_total_class(self, class_type):
+        """
+        Metodo per ritornare il numero totale di job in una certa classe nel sistema
+        """
+        if class_type > 3:
+            raise ValueError(error)
+        return sum([self.matrix[i][class_type] for i in range(3)])
+    
+class Server():
+    """
+    Classe che definisce un server all'interno di un nodo. Il server ha una capacità
+    e una distribuzione di servizio.
+    id: identificativo del server, può essere A, B, P
+    capacity: capacità del server
+    server_distribution: tupla con distribuzione di servizio e parametro della distribuzione
+    """
+    def __init__(self, id: str, capacity: int, server_distribution: str):
+        self.id = id
+        self.capacity = capacity
+        self.server_distribution = server_distribution
+
+    def node_map(self, node_id):
+        server_map = {'A': 0, 'B': 1, 'P': 2}
+        if node_id not in server_map:
+            raise ValueError(error)
+        return server_map[node_id]
+        
+class Queue():
+    def __init__(self, capacity: int, queue_policy='FIFO'):
+        self.id = id
+        self.capacity = capacity
+        self.queue_policy = queue_policy
+
+class Node:
+    def __init__(self, id: str, service_rate: list, server:Server, queue:Queue):
+        self.id = id
+        self.server = server
+        self.queue = queue
+        self.service_rate = service_rate
+
+    def get_service_class_rate(self, class_type):
+        if class_type > 3:
+            raise ValueError(error)
+        
+        return self.service_rate[class_type]
 
 
+class Network:
+    def __init__(self, nodes: list, state: State):
+        self.nodes = nodes
+        self.state = state
+
+    def get_node(self, node_id):
+        for node in self.nodes:
+            if node.id == node_id:
+                return node
+        return None
+
+    def get_state(self):
+        return self.state.get()
+
+    def get_node_state(self, node_id):
+        node = self.get_node(node_id)
+        return self.state.get_node_state(node.id)
+
+    def get_total_class(self, class_type):
+        return self.state.get_total_class(class_type)
--- src/caballo/domestico/wwsimulator/nextevent/handlers.py
+++ src/caballo/domestico/wwsimulator/nextevent/handlers.py
@@ -1,30 +1,64 @@
-from caballo.domestico.wwsimulator.nextevent.events import EventContext, EventHandler
+from caballo.domestico.wwsimulator.nextevent.events import EventContext, EventHandler, Event, ArrivalEvent, DepartureEvent
+from caballo.domestico.wwsimulator.model import Job, Server
+import caballo.domestico.wwsimulator.des.rvgs as des
 
 class HandleArrival(EventHandler):
     def __init__(self):
         super().__init__()
 
     def _handle(self, context: EventContext):
-        # TODO: implement arrival handling
-        #
-        # Example:
-        #
-        # Arrival job j classe_1 nel server A 	// entrata nel sistema 
-        # N_A_1 += 1 
-        # Schedula Departure con servizio classe 1 per job j da server A 
-        # Schedula Arrival con delay 0 classe 1 per job j+1 in Server A  
-        pass
+        job_class = context.event.job.class_id
+        job_id = context.event.job.job_id
+        job_server = context.event.server.id
+        job_server_int = context.event.server.node_map(job_server)
+        # TODO: rimuovere limite job
+        if job_id < 10:
+            print(f"Arrival of job {job_id} of class {job_class} at server {job_server}:{job_server_int}")
+            # aggiornamento dello stato del sistema
+            context.network.state.update((job_server_int, job_class), True)
+
+            # generazione evento di departure per il job corrente
+            service_rate = context.network.nodes[job_server_int].service_rate[job_class]
+            service_time = des.Exponential(service_rate)
+            departure = DepartureEvent(context.event.time + service_time, HandleDeparture(), context.event.job, context.event.server)
+
+            # scheduling dell'evento di departure
+            context.scheduler.schedule(departure)
+
+            # rigenerazione evento di arrival dall'esterno del sistema
+            if job_class == 0:
+                # TODO: parametrizzare il tasso di arrivo
+                arrival_time = des.Poisson(0.5)
+                new_job = Job(0, context.event.job.job_id+1)
+                arrival = ArrivalEvent(context.event.time + arrival_time, HandleArrival(), new_job, context.event.server)
+                context.scheduler.schedule(arrival)
+        
 
 class HandleDeparture(EventHandler):
     def __init__(self):
         super().__init__()
 
     def _handle(self, context: EventContext):
-        # TODO: implement departure handling
-        #
-        # Example:
-        #
-        # Departure job j Classe_1 server A 
-        # N_A_1 -= 1 
-        # Schedula Arrivo con delay A->B  j  in server B 
-        pass
+        job_server_str = context.event.server.id
+        job_server = context.event.server.node_map(job_server_str)
+        job_class = context.event.job.class_id
+    
+        print(f"Departure of job {context.event.job.job_id} of class {job_class} from server {job_server_str}:{job_server}")
+
+        # aggiornamento dello stato del sistema
+        context.network.state.update((job_server, job_class), False)
+
+        # generazione evento di arrival successivo
+        context.event.job.class_id = job_class+1 if job_server_str != 'A' or job_class != 0 else job_class
+        if job_class < 2:
+            if job_server_str in ['B', 'P']:
+                new_server_id = 'A'
+            elif job_server_str == 'A' and job_class == 0:
+                new_server_id = 'B'
+            else:
+                new_server_id = 'P'
+            next_server = Server(new_server_id, 100, 'exp')
+            arrival = ArrivalEvent(context.event.time, HandleArrival(), context.event.job, next_server)
+            context.scheduler.schedule(arrival)
+         
+        
